1d59e48c18c02729bf558da9902fa5b56944d48e
Alex Klos <alexklos@fastmail.com>
2021-02-20 17:01:05 +0000

    fix safe loop

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ import urllib3
 import socket
 import binance
 
-def safe_loop():
+def safe_loop(loop_count = 0):
     try:
         t = Trader()
         print('$', t.portfolio['balance'])
         t.scan_market_loop()
     except Exception as e:
         print(e)
-        if type(e).__name__ != 'KeyboardInterrupt':
-            safe_loop()
+        if type(e).__name__ != 'KeyboardInterrupt' and loop_count < 10:
+            safe_loop(loop_count + 1)
 
 if __name__ == '__main__':
     safe_loop()
